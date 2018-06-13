4b0c06205614191147cf5f31469a8091cc68598d
Long Yu Wang <mikewangly@gmail.com>
2018-06-13 11:18:54 -0400

    clear data on init_db

--- database.py
+++ database.py
@@ -21,4 +21,5 @@ def init_db():
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import models
+    Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
